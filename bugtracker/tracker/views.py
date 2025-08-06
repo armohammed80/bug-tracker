7af6eb7162400c137ec17b730f14222f34e35904
Atiqur Rahman Mohammed <armohammed80@gmail.com>
2025-08-06 17:39:14 +0400

    Add registration

--- bugtracker/tracker/views.py
+++ bugtracker/tracker/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from .forms import BugForm, CommentForm, ProjectForm
+from django.contrib.auth.models import Group
+from django.contrib.auth import login
 from .models import Bug, Project
 
 
@@ -63,3 +66,18 @@ def create_project(request):
     else:
         form = ProjectForm()
     return render(request, 'tracker/project_form.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        role = request.POST.get('role')
+        if form.is_valid() and role in ['Developer', 'Tester']:
+            user = form.save()
+            group = Group.objects.get(name=role)
+            user.groups.add(group)
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'tracker/register.html', {'form': form})
