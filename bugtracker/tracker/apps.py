68d03de10471f689225a104c565bce9480abc6c4
Atiqur Rahman Mohammed <armohammed80@gmail.com>
2025-08-07 10:58:00 +0400

    Automatically assign groups permissions to groups

--- bugtracker/tracker/apps.py
+++ bugtracker/tracker/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class TrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tracker'
+
+    def ready(self):
+        from .signals import create_roles_and_permissions
+        create_roles_and_permissions()
---
+++ bugtracker/tracker/signals.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.db.utils import OperationalError
+from django.apps import apps
+
+def create_roles_and_permissions():
+    try:
+        Bug = apps.get_model('tracker', 'Bug')
+        Project = apps.get_model('tracker', 'Project')
+        Comment = apps.get_model('tracker', 'Comment')
+        UserProfile = apps.get_model('tracker', 'UserProfile')
+        Organization = apps.get_model('tracker', 'Organization')
+
+        models_permissions = {
+            'Developer': {
+                Bug: ['view', 'change'],
+                Comment: ['view', 'add'],
+                Project: ['view'],
+            },
+            'Tester': {
+                Bug: ['view', 'add'],
+                Comment: ['view', 'add'],
+                Project: ['view'],
+            },
+            'Admin': {
+                Bug: ['view', 'add', 'change', 'delete'],
+                Comment: ['view', 'add', 'change', 'delete'],
+                Project: ['view', 'add', 'change', 'delete'],
+                UserProfile: ['view'],
+                Organization: ['view'],
+            }
+        }
+
+        for role, perms in models_permissions.items():
+            group, _ = Group.objects.get_or_create(name=role)
+
+            for model, actions in perms.items():
+                ctype = ContentType.objects.get_for_model(model)
+                for action in actions:
+                    codename = f"{action}_{model._meta.model_name}"
+                    try:
+                        perm = Permission.objects.get(codename=codename, content_type=ctype)
+                        group.permissions.add(perm)
+                    except Permission.DoesNotExist:
+                        print(f"Permission {codename} not found.")
+
+    except OperationalError:
+        pass
