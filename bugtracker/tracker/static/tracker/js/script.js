9473c943830ed2fca448c0bc14d72ba079d07bc7
Atiqur Rahman Mohammed <armohammed80@gmail.com>
2025-08-22 15:58:33 +0400

    Remove unnecessary alert.

--- bugtracker/tracker/static/tracker/js/script.js
+++ bugtracker/tracker/static/tracker/js/script.js
@@ -1,10 +1,3 @@
-// Show a "form submitted" alert
-document.querySelectorAll('form').forEach(form => {
-    form.addEventListener('submit', () => {
-        alert("Your changes have been submitted!");
-    });
-});
-
 // Live character count for textareas
 document.querySelectorAll('textarea').forEach(textarea => {
     const counter = document.createElement('small');
